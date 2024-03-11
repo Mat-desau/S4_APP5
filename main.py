524096c3c32199f7acc5ccbe4dbb6262238e1351
Mat-desau <115102447+Mat-desau@users.noreply.github.com>
2024-03-11 19:32:06 -0400

    FUK THIS C'EST POCHE

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #Libraries utiliser
 import numpy as np
 import wave
+import matplotlib.pyplot as plt
+
+#Boolean pour les fonctions
+Afficher_Graphique = True
+
+#Valeur utiles
+pi = np.pi
+
 
 def LectureToArray(NomFichier):
     # Read file to get buffer
@@ -15,11 +23,32 @@ def LectureToArray(NomFichier):
     return Audio_float32
 
 def Trouver32Sinus(Array):
-    
+    return Array
 
 def main():
+    #Lecture des audio et mise en array
     Audio_Guitare = LectureToArray("note_guitare_LAd.wav")
     Audio_Basson = LectureToArray("note_basson_plus_sinus_1000_Hz.wav")
+
+    #Redressement des audios
+    Audio_Guitare_Redresser = abs(Audio_Guitare)
+    Audio_Basson_Redresser = abs(Audio_Basson)
+
+    #Afficher sur les graphique au besoin
+    if(Afficher_Graphique):
+        Figure1, (SUB1, SUB2) = plt.subplots(2, 1)
+        SUB1.plot(Audio_Guitare)
+        SUB1.set_title('Audio Guitare Normal')
+        SUB2.plot(Audio_Guitare_Redresser)
+        SUB2.set_title('Audio Guitare Redresser')
+
+        Figure2, (SUB3, SUB4) = plt.subplots(2, 1)
+        SUB3.plot(Audio_Basson)
+        SUB3.set_title('Audio Basson Normal')
+        SUB4.plot(Audio_Basson_Redresser)
+        SUB4.set_title('Audio Basson Redresser')
+
+        plt.show()
 
 
 
