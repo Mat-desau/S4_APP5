dc1b250915e667e2d6c50f5198c9e72eff246ac0
Mat-desau <115102447+Mat-desau@users.noreply.github.com>
2024-03-15 11:16:02 -0400

    Aucune idee on est rendu où caa marche i guess

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from scipy.fft import fft
 import librosa as librosa
 
 #Boolean pour les fonctions
-Afficher_Graphique = False
+Afficher_Graphique = True
+Afficher_Filtres = False
 Mise_A_Base_1 = True
 Mise_En_Log = True
 
@@ -33,32 +34,54 @@ def LectureToArray(NomFichier):
 def Trouver32Sinus(Array, Sample_Rate):
     #Pour cree les harmoniques
     Array_Hanning = Array * np.hanning(len(Array))
-    Signal_FFT = fft(Array_Hanning)
+    Signal_FFT = fft(Array_Hanning)   #Array FFT
 
-    Signal_FFT_New = Signal_FFT[0:int(len(Signal_FFT)/2)]
+    #Garder juste coté positif
+    Signal_FFT_New = Signal_FFT[0:int(len(Signal_FFT)/2)]   #Juste coté positif
+    Signal_FFT_New_Not_db = Signal_FFT  # Juste coté positif
 
-    #yoyo
-
+    #Mise des données en LOG
     if(Mise_En_Log):
-        Signal_FFT_New = 20 * np.log10(np.abs(Signal_FFT_New))
+        Signal_FFT_New = 20 * np.log10(np.abs(Signal_FFT_New)) #Mise en log
 
     #Normaliser les frequences
-    Frequence_New = np.linspace(0, 0.5, len(Signal_FFT_New)) * Sample_Rate
+    Frequence_New = np.linspace(0, 0.5, len(Signal_FFT_New)) * Sample_Rate  #Creation de la fréquence qui est en fonction du sample rate
+    Frequence_Full = np.linspace(0, 1, len(Signal_FFT_New_Not_db)) * Sample_Rate
 
     #find peaks en utilisant la valeur maximale comme distance
-    Position_Maximum, _ = signal.find_peaks(Signal_FFT_New, distance=np.argmax(Signal_FFT_New))
-    Position_Maximum2, _ = signal.find_peaks(Signal_FFT_New, height=(0.85 * np.max(Signal_FFT_New)), distance=40)
+    Position_Maximum, _ = signal.find_peaks(Signal_FFT_New, distance=np.argmax(Signal_FFT_New)) #Find peaks en fonction du plus haut (Position en X)
+    Position_Maximum2, _ = signal.find_peaks(Signal_FFT_New, height=(0.85 * np.max(Signal_FFT_New)), distance=40)   #Trouver Max en fonction de 10% (Position en X)
 
+    #Ajustement pour Basson
     if(Position_Maximum2[0] < Position_Maximum[0]):
-        Position_Maximum, _ = signal.find_peaks(Signal_FFT_New, distance = (Position_Maximum2[1] - Position_Maximum2[0]))
+        Position_Maximum, _ = signal.find_peaks(Signal_FFT_New, distance = (Position_Maximum2[1] - Position_Maximum2[0]))   #Si jamais il a un plus petit avant le plus haut
 
     #Position_Maximum, _ = signal.find_peaks(Signal_FFT_New, distance=np.argmax(Signal_FFT_New))
     #Position_Maximum, _ = signal.find_peaks(Signal_FFT_New, )
-    Position_Maximum = Position_Maximum[:32]
 
-    Freq_Max = Frequence_New[Position_Maximum]
+    #Garder juste les 32 premiers
+    Position_Maximum = Position_Maximum[:32]    #Juste garder les 32 premiers index des max
 
-    return Frequence_New, Signal_FFT_New, Position_Maximum, Freq_Max
+    Freq_Max = Frequence_New[Position_Maximum]  #Trouver les fréquences maximum et non juste les indexs
+
+    return Frequence_New, Frequence_Full, Signal_FFT_New, Signal_FFT_New_Not_db, Position_Maximum, Freq_Max
+
+def Trouver_Enveloppe(Signal, Attente, Fstop, Fpass):
+    N = Calcul_N(Attente, Fstop, Fpass)   #Calcul du N avec la fonction du manuel
+    Passe_Bas_Valeur = Passe_Bas(2*len(Signal), int(N))   #Filtre passe Bas
+    Enveloppe = Passe_Bas_Valeur * Signal   #Trouver l'enveloppe
+    Enveloppe_Temps = np.fft.ifft(Enveloppe)    #Inverse pour la mettre en fontion du temps au lieu d'en fontion des fréquences
+
+    return Enveloppe, Enveloppe_Temps, Passe_Bas_Valeur
+
+def Calcul_N(Atten, Fstop_db, Fpass_db):
+    return Atten / (22 * (Fstop_db - Fpass_db)) #Formule du manuel
+
+def Passe_Bas(Len_Array, N):
+    h = np.ones(N) * (1/N)
+    h = fft(h, n=Len_Array)
+    h = h[0:int(len(h)/2)]
+    return h
 
 def Coupe_Bande(Array, Sample_Rate, N):
     w0 = 2 * pi * 1000 #en rad
@@ -83,7 +106,20 @@ def Coupe_Bande(Array, Sample_Rate, N):
     SUB2.plot(k, h)
     plt.show()
 
-def plot(X1, X2, Titre1, Titre2):
+def plot1(X1, Y1, Titre1):
+    Figure1, SUB1 = plt.subplots(1, 1)
+    SUB1.plot(X1, Y1)
+    SUB1.set_title(Titre1)
+    plt.show()
+
+def plot1_2(X1, Y1, X2, Y2, Titre1):
+    Figure1, SUB1 = plt.subplots(1, 1)
+    SUB1.plot(X1, Y1)
+    SUB1.plot(X2, "X", color='red')
+    SUB1.set_title(Titre1)
+    plt.show()
+
+def plot2(X1, X2, Titre1, Titre2):
     Figure1, (SUB1, SUB2) = plt.subplots(2, 1)
     SUB1.plot(X1)
     SUB1.set_title(Titre1)
@@ -91,7 +127,7 @@ def plot(X1, X2, Titre1, Titre2):
     SUB2.set_title(Titre2)
     plt.show()
 
-def plot2(X1, Y1, X2, Y2, Titre1, Titre2):
+def plot2_2(X1, Y1, X2, Y2, Titre1, Titre2):
     Figure1, (SUB1, SUB2) = plt.subplots(2, 1)
     SUB1.plot(X1, Y1)
     SUB1.set_title(Titre1)
@@ -118,19 +154,30 @@ def main():
     Audio_Guitare_Redresser = abs(Audio_Guitare)
     Audio_Basson_Redresser = abs(Audio_Basson)
 
-    Coupe_Bande(Audio_Basson, Sample_Rate_Basson, 4096)
+    #Filtres
+    #Coupe_Bande(Audio_Basson, Sample_Rate_Basson, 4096)
 
-    Frequence_Guitare, Signal_FFT_Guitare, Positon_Maximum_Guitare, Frequence_Maximum_Guitare = Trouver32Sinus(Audio_Guitare, Sample_Rate_Guitare)
+    #Trouver les 32 frequences
+    Frequence_Guitare, Frequence_FFT_Full_Guitare, Signal_FFT_Guitare, Signal_FFT_Not_db_Guitare, Positon_Maximum_Guitare, Frequence_Maximum_Guitare = Trouver32Sinus(Audio_Guitare, Sample_Rate_Guitare)
+    Frequence_Basson, Frequence_FFT_Full_Basson, Signal_FFT_Basson, Signal_FFT_Not_db_Basson, Positon_Maximum_Basson, Frequence_Maximum_Basson = Trouver32Sinus(Audio_Basson, Sample_Rate_Basson)
 
-    Frequence_Basson, Signal_FFT_Basson, Positon_Maximum_Basson, Frequence_Maximum_Basson = Trouver32Sinus(Audio_Basson, Sample_Rate_Basson)
+    #Enveloppe
+    Enveloppe_Guitare, Enveloppe_Temps_Guitare, Passe_Bas_Valeur_Guitare = Trouver_Enveloppe(Signal_FFT_Not_db_Guitare, 3, (pi/1000)/(2*pi), 0) #fois 2pi pour le mettre en Hz, puisque le reste est en Hertz
+    Enveloppe_Basson, Enveloppe_Temps_Basson, Passe_Bas_Valeur_Basson = Trouver_Enveloppe(Signal_FFT_Not_db_Basson, 3, (pi/1000)/(2*pi), 0)
+
+    # Affichage
+    if (Afficher_Filtres):
+        plot1(Frequence_Guitare, np.abs(Passe_Bas_Valeur_Guitare), 'Filte Passe-Bas Guitare')
+        plot1(Frequence_Basson, np.abs(Passe_Bas_Valeur_Basson), 'Filte Passe-Bas Basson')
 
     #Afficher sur les graphique au besoin
     if(Afficher_Graphique):
-
-        plot(Audio_Guitare, Audio_Guitare_Redresser, 'Audio Guitare Normal', 'Audio Guitare Redresser')
-        plot(Audio_Basson, Audio_Basson_Redresser, 'Audio Basson Normal', 'Audio Basson Redresser')
-        #plot2(Frequence_Guitare, Signal_FFT_Guitare, Frequence_Basson, Signal_FFT_Basson, 'Harmoniques Guitare', 'Harmoniques Basson')
+        plot2(Audio_Guitare, Audio_Guitare_Redresser, 'Audio Guitare Normal', 'Audio Guitare Redresser')
+        plot2(Audio_Basson, Audio_Basson_Redresser, 'Audio Basson Normal', 'Audio Basson Redresser')
+        #plot2_2(Frequence_Guitare, Signal_FFT_Guitare, Frequence_Basson, Signal_FFT_Basson, 'Harmoniques Guitare', 'Harmoniques Basson')
         plot3(Frequence_Guitare, Signal_FFT_Guitare, Frequence_Basson, Signal_FFT_Basson, Positon_Maximum_Guitare, Signal_FFT_Guitare, Positon_Maximum_Basson, Signal_FFT_Basson, 'Harmoniques Guitare', 'Harmoniques Basson')
+        plot2_2(Frequence_FFT_Full_Guitare, np.abs(Enveloppe_Guitare), Frequence_FFT_Full_Guitare, np.abs(Enveloppe_Temps_Guitare), 'Enveloppe Fréquence Guitare', 'Enveloppe Temps Guitare')
+        plot2_2(Frequence_FFT_Full_Basson, np.abs(Enveloppe_Basson), Frequence_FFT_Full_Basson, np.abs(Enveloppe_Temps_Basson), 'Enveloppe Fréquence Basson', 'Enveloppe Temps Basson')
 
 if __name__ == '__main__':
     main()
